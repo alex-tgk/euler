ab1674c6a6bea35eb36ad6fc20d8e375bf5f3f42
Alex Carroll <alexandertcarroll@gmail.com>
2020-02-13 21:05:30 -0500

    Problem 2 solved

---
+++ problem-3/main.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+#
+#   The prime factors of 13195 are 5, 7, 13 and 29.
+#   What is the largest prime factor of the number 600851475143?
+#
+
+import sys
+
+def main():
+    
+
+if __name__ == '__main__':
+  main()
